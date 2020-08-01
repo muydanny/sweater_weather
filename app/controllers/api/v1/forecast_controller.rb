e5e3610d9e8988ca9622263cd74db18b9de9af4a
Danny Ramos <danramos21@gmail.com>
2020-08-01 16:34:34 -0600

    refactor to forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::ForecastController < ApplicationController
 
   def index
-    require 'pry'; binding.pry
-    location = params[:location]
+    # location_info =
+    map_info = MapquestService.new.get_directions(params[:location])
+    open_weather_info = OpenWeatherService.new.get_forecast(map_info)
+    # location = params[:location]
+    # conn = Faraday.new(url: "api.openweathermap.org") do |faraday|
+    #   faraday.params["appid"] = ENV["OPEN_WEATHER_KEY"]
+    # end
 
-    conn = Faraday.new(url: "api.openweathermap.org") do |faraday|
-      faraday.params["appid"] = ENV["OPEN_WEATHER_KEY"]
-    end
-
-    response = conn.get("/data/2.5/onecall?#{location}")
+    # response = conn.get("/data/2.5/onecall?#{location}")
   end
 end
