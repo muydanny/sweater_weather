055186ab8e41385868f833ead10693c561bf8345
Danny Ramos <danramos21@gmail.com>
2020-08-02 11:09:45 -0600

    changed spec to expect data from cassette

--- spec/requests/api/v1/forecast_location_spec.rb
+++ spec/requests/api/v1/forecast_location_spec.rb
@@ -9,26 +9,23 @@ describe "Forecast via location API" do
     
     body = JSON.parse(response.body)
 
-    # set up VCR
-    # and actually assert on the values
-
     expect(body["data"]["attributes"]["current_weather_description"]).to eq("scattered clouds")
-    expect(body["data"]["attributes"]["current_temp"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_date_and_time"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_high"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_low"]).not_to be(nil)
-    expect(body["data"]["attributes"]["feels_like"]).not_to be(nil)
-    expect(body["data"]["attributes"]["humidity"]).not_to be(nil)
-    expect(body["data"]["attributes"]["visibility"]).not_to be(nil)
-    expect(body["data"]["attributes"]["uv_index"]).not_to be(nil)
-    expect(body["data"]["attributes"]["sunrise"]).not_to be(nil)
-    expect(body["data"]["attributes"]["sunset"]).not_to be(nil)
-    expect(body["data"]["attributes"]["hourly_time"]).not_to be(nil)
-    expect(body["data"]["attributes"]["hourly_temp"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_weather_description"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_precipitation"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_high"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_low"]).not_to be(nil)
+    expect(body["data"]["attributes"]["current_temp"]).to eq(72.99)
+    expect(body["data"]["attributes"]["current_date_and_time"]).to eq(1596386602)
+    expect(body["data"]["attributes"]["current_high"]).to eq(83.34)
+    expect(body["data"]["attributes"]["current_low"]).to eq(72.18)
+    expect(body["data"]["attributes"]["feels_like"]).to eq(70.45)
+    expect(body["data"]["attributes"]["humidity"]).to eq(56)
+    expect(body["data"]["attributes"]["visibility"]).to eq(10000)
+    expect(body["data"]["attributes"]["uv_index"]).to eq(10.3)
+    expect(body["data"]["attributes"]["sunrise"]).to eq(1596369620)
+    expect(body["data"]["attributes"]["sunset"]).to eq(1596420710)
+    expect(body["data"]["attributes"]["hourly_time"]).to eq(1596384000)
+    expect(body["data"]["attributes"]["hourly_temp"]).to eq(72.99)
+    expect(body["data"]["attributes"]["daily_weather_description"]).to eq("scattered clouds")
+    expect(body["data"]["attributes"]["daily_precipitation"]).to eq(0)
+    expect(body["data"]["attributes"]["daily_high"]).to eq(83.34)
+    expect(body["data"]["attributes"]["daily_low"]).to eq(72.18)
   end
   
 end
