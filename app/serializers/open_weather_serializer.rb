c964c0d02e45881409ec3d3014005fada1b73807
Danny Ramos <danramos21@gmail.com>
2020-08-02 10:19:43 -0600

    set id for fastjsonapi

--- app/serializers/open_weather_serializer.rb
+++ app/serializers/open_weather_serializer.rb
@@ -1,4 +1,5 @@
 class OpenWeatherSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :current_weather_description
+  set_id :id
+  attributes :current_weather_description, :current_temp, :current_date_and_time, :current_high, :current_low, :feels_like, :humidity, :visibility, :uv_index, :sunrise, :sunset, :hourly_time, :hourly_temp, :daily_weather_description, :daily_precipitation, :daily_high, :daily_low
 end
