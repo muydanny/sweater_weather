172e8925c9fb2aa4be96b20bc34c4d1c9abe070e
Danny Ramos <danramos21@gmail.com>
2020-08-02 14:22:55 -0600

    background image route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get 'forecast', controller: 'forecast', action: 'index'
+      get 'background', controller: 'background', action: 'show'
     end
   end
 end
