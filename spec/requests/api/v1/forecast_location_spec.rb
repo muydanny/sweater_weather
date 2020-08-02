a94d624b319a61c52dc3e0a5bd3b8d2b24459cf4
Danny Ramos <danramos21@gmail.com>
2020-08-02 10:44:59 -0600

    vcr requirements

--- spec/requests/api/v1/forecast_location_spec.rb
+++ spec/requests/api/v1/forecast_location_spec.rb
@@ -2,32 +2,33 @@ require 'rails_helper'
 
 describe "Forecast via location API" do
   it "can get forecast for a location" do
+    VCR.use_cassette('forecast_for_city') do
+      get "/api/v1/forecast?location=denver,co"
 
-    get "/api/v1/forecast?location=denver,co"
+      expect(response).to be_successful
+      
+      body = JSON.parse(response.body)
 
-    expect(response).to be_successful
-    
-    body = JSON.parse(response.body)
+      # set up VCR
+      # and actually assert on the values
 
-    # set up VCR
-    # and actually assert on the values
-
-    expect(body["data"]["attributes"]["current_weather_description"]).to eq("overcast clouds")
-    expect(body["data"]["attributes"]["current_temp"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_date_and_time"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_high"]).not_to be(nil)
-    expect(body["data"]["attributes"]["current_low"]).not_to be(nil)
-    expect(body["data"]["attributes"]["feels_like"]).not_to be(nil)
-    expect(body["data"]["attributes"]["humidity"]).not_to be(nil)
-    expect(body["data"]["attributes"]["visibility"]).not_to be(nil)
-    expect(body["data"]["attributes"]["uv_index"]).not_to be(nil)
-    expect(body["data"]["attributes"]["sunrise"]).not_to be(nil)
-    expect(body["data"]["attributes"]["sunset"]).not_to be(nil)
-    expect(body["data"]["attributes"]["hourly_time"]).not_to be(nil)
-    expect(body["data"]["attributes"]["hourly_temp"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_weather_description"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_precipitation"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_high"]).not_to be(nil)
-    expect(body["data"]["attributes"]["daily_low"]).not_to be(nil)
+      expect(body["data"]["attributes"]["current_weather_description"]).to eq("scattered clouds")
+      expect(body["data"]["attributes"]["current_temp"]).not_to be(nil)
+      expect(body["data"]["attributes"]["current_date_and_time"]).not_to be(nil)
+      expect(body["data"]["attributes"]["current_high"]).not_to be(nil)
+      expect(body["data"]["attributes"]["current_low"]).not_to be(nil)
+      expect(body["data"]["attributes"]["feels_like"]).not_to be(nil)
+      expect(body["data"]["attributes"]["humidity"]).not_to be(nil)
+      expect(body["data"]["attributes"]["visibility"]).not_to be(nil)
+      expect(body["data"]["attributes"]["uv_index"]).not_to be(nil)
+      expect(body["data"]["attributes"]["sunrise"]).not_to be(nil)
+      expect(body["data"]["attributes"]["sunset"]).not_to be(nil)
+      expect(body["data"]["attributes"]["hourly_time"]).not_to be(nil)
+      expect(body["data"]["attributes"]["hourly_temp"]).not_to be(nil)
+      expect(body["data"]["attributes"]["daily_weather_description"]).not_to be(nil)
+      expect(body["data"]["attributes"]["daily_precipitation"]).not_to be(nil)
+      expect(body["data"]["attributes"]["daily_high"]).not_to be(nil)
+      expect(body["data"]["attributes"]["daily_low"]).not_to be(nil)
+    end
   end
 end
