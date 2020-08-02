bfd20bffa25f1e7b870c06740ed2175844850ca3
Danny Ramos <danramos21@gmail.com>
2020-08-02 10:48:59 -0600

    made my vcr life much easier

--- spec/requests/api/v1/forecast_location_spec.rb
+++ spec/requests/api/v1/forecast_location_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 describe "Forecast via location API" do
-  it "can get forecast for a location" do
-    VCR.use_cassette('forecast_for_city') do
-      get "/api/v1/forecast?location=denver,co"
+  it "can get forecast for a location", :vcr do
 
-      expect(response).to be_successful
-      
-      body = JSON.parse(response.body)
+    get "/api/v1/forecast?location=denver,co"
 
-      # set up VCR
-      # and actually assert on the values
+    expect(response).to be_successful
+    
+    body = JSON.parse(response.body)
 
-      expect(body["data"]["attributes"]["current_weather_description"]).to eq("scattered clouds")
-      expect(body["data"]["attributes"]["current_temp"]).not_to be(nil)
-      expect(body["data"]["attributes"]["current_date_and_time"]).not_to be(nil)
-      expect(body["data"]["attributes"]["current_high"]).not_to be(nil)
-      expect(body["data"]["attributes"]["current_low"]).not_to be(nil)
-      expect(body["data"]["attributes"]["feels_like"]).not_to be(nil)
-      expect(body["data"]["attributes"]["humidity"]).not_to be(nil)
-      expect(body["data"]["attributes"]["visibility"]).not_to be(nil)
-      expect(body["data"]["attributes"]["uv_index"]).not_to be(nil)
-      expect(body["data"]["attributes"]["sunrise"]).not_to be(nil)
-      expect(body["data"]["attributes"]["sunset"]).not_to be(nil)
-      expect(body["data"]["attributes"]["hourly_time"]).not_to be(nil)
-      expect(body["data"]["attributes"]["hourly_temp"]).not_to be(nil)
-      expect(body["data"]["attributes"]["daily_weather_description"]).not_to be(nil)
-      expect(body["data"]["attributes"]["daily_precipitation"]).not_to be(nil)
-      expect(body["data"]["attributes"]["daily_high"]).not_to be(nil)
-      expect(body["data"]["attributes"]["daily_low"]).not_to be(nil)
-    end
+    # set up VCR
+    # and actually assert on the values
+
+    expect(body["data"]["attributes"]["current_weather_description"]).to eq("scattered clouds")
+    expect(body["data"]["attributes"]["current_temp"]).not_to be(nil)
+    expect(body["data"]["attributes"]["current_date_and_time"]).not_to be(nil)
+    expect(body["data"]["attributes"]["current_high"]).not_to be(nil)
+    expect(body["data"]["attributes"]["current_low"]).not_to be(nil)
+    expect(body["data"]["attributes"]["feels_like"]).not_to be(nil)
+    expect(body["data"]["attributes"]["humidity"]).not_to be(nil)
+    expect(body["data"]["attributes"]["visibility"]).not_to be(nil)
+    expect(body["data"]["attributes"]["uv_index"]).not_to be(nil)
+    expect(body["data"]["attributes"]["sunrise"]).not_to be(nil)
+    expect(body["data"]["attributes"]["sunset"]).not_to be(nil)
+    expect(body["data"]["attributes"]["hourly_time"]).not_to be(nil)
+    expect(body["data"]["attributes"]["hourly_temp"]).not_to be(nil)
+    expect(body["data"]["attributes"]["daily_weather_description"]).not_to be(nil)
+    expect(body["data"]["attributes"]["daily_precipitation"]).not_to be(nil)
+    expect(body["data"]["attributes"]["daily_high"]).not_to be(nil)
+    expect(body["data"]["attributes"]["daily_low"]).not_to be(nil)
   end
+  
 end
