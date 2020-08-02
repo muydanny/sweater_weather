3ca086d9750b30c7674d4ca73f2856be4e5b6171
Danny Ramos <danramos21@gmail.com>
2020-08-01 18:32:40 -0600

    added weather info poro

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,13 +2,9 @@ class Api::V1::ForecastController < ApplicationController
 
   def index
     map_info = MapquestService.new.get_lat_long(params[:location])
-
     open_weather_info = OpenWeatherService.new.get_forecast(map_info)
-    # location = params[:location]
-    # conn = Faraday.new(url: "api.openweathermap.org") do |faraday|
-    #   faraday.params["appid"] = ENV["OPEN_WEATHER_KEY"]
-    # end
-
-    # response = conn.get("/data/2.5/onecall?#{location}")
+    render json: open_weather_info
+    
+    # render json: OpenWeatherSerializer.new(WeatherInfo(open_weather_info)).serialized_json
   end
 end
