5e3cebe59cf3b3c0beb6f74d314c26c886ede5a2
Danny Ramos <danramos21@gmail.com>
2020-08-03 10:01:52 -0600

    added attr_reader to poro

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -1,13 +1,17 @@
 class Trail
 
-
+  attr_reader :trail_id,
+              :trail_name,
+              :trail_summary,
+              :trail_difficulty,
+              :trail_location
 
   def initialize(trail_info)
-    require 'pry'; binding.pry
+    @trail_id = trail_info[:trails][0][:id]
     @trail_name = trail_info[:trails][0][:name]
     @trail_summary = trail_info[:trails][0][:summary]
     @trail_difficulty = trail_info[:trails][0][:difficulty]
-    @trail_locaiton = trail_info[:trails][0][:location]
-    
+    @trail_location = trail_info[:trails][0][:location]
+    #NOTE: can use the trail location to find distance from starting location
   end
 end
