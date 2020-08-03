419399bc9e046e5162c66e0853eccab5b76fb61d
Danny Ramos <danramos21@gmail.com>
2020-08-03 10:37:00 -0600

    current weather attributes added to trail poro

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -12,6 +12,8 @@ class Trail
     @trail_summary = trail_info[:trails][0][:summary]
     @trail_difficulty = trail_info[:trails][0][:difficulty]
     @trail_location = trail_info[:trails][0][:location]
+    @current_location_weather_summary = weather_info[:current][:weather][0][:description]
+    @current_location_temp = weather_info[:current][:temp]
     #NOTE: can use the trail location to find distance from starting location
   end
 end
