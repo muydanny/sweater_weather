480b0882b1abb77652dd5604562458326620ad95
Danny Ramos <danramos21@gmail.com>
2020-08-03 09:46:16 -0600

    getting correct response

--- app/services/hiking_project_service.rb
+++ app/services/hiking_project_service.rb
@@ -1,16 +1,16 @@
 class HikingProjectService
   def get_trails(location)
     response = conn.get("/data/get-trails?lat=#{location[:lat]}&lon=#{location[:lng]}") do |req|
-      req.params["key"] = ENV["HIKING_PROJECT_KEY"]
-      req.params["location"] = location
-    end
+    req.params["key"] = ENV["HIKING_PROJECT_KEY"]
+  end
+    require 'pry'; binding.pry
     JSON.parse(response.body, symbolize_names: true)
   end
 
   private
 
   def conn
-    Faraday.new("https://api.openweathermap.org") do |req|
+    Faraday.new("https://www.hikingproject.com") do |req|
       req.params["key"] = ENV["HIKING_PROJECT_KEY"]
     end
   end
