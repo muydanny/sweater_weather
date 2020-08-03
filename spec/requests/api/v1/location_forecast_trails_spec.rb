43116a3c8f3c69fd49a5b0624f05c71707ac30c9
Danny Ramos <danramos21@gmail.com>
2020-08-03 11:41:20 -0600

    distance spec passing

--- spec/requests/api/v1/location_forecast_trails_spec.rb
+++ spec/requests/api/v1/location_forecast_trails_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Forecast for nearby trails" do
-  it "can get a trails for a location", :vcr do
+describe "Retrieve the forecast for a location and nearby trails" do
+  it "can get trails for a location", :vcr do
 
     get "/api/v1/trails?location=denver,co"
     expect(response).to be_successful  
@@ -19,22 +19,15 @@ describe "Forecast for nearby trails" do
     expect(response).to be_successful  
     body = JSON.parse(response.body)
     
-    expect(body["data"]["attributes"]["current_location_weather_summary"]).to eq("clear sky")
-    expect(body["data"]["attributes"]["current_location_temp"]).to eq(73.18)
+    expect(body["data"]["attributes"]["current_location_weather_summary"]).to eq("few clouds")
+    expect(body["data"]["attributes"]["current_location_temp"]).to eq(76.6)
   end
 
-  # it "can get the distance to the trail" do 
-  #   get "/api/v1/trails?location=denver,co"
-  #   expect(response).to be_successful  
-  #   body = JSON.parse(response.body)
+  it "can get the distance to the trail", :vcr do 
+    get "/api/v1/trails?location=denver,co"
+    expect(response).to be_successful  
+    body = JSON.parse(response.body)
     
-    
-  # end
+    expect(body["data"]["attributes"]["distance_to_trail"]).to eq(880.5239)
+  end
 end
-
-
-
-# You will build an endpoint that will retrieve the forecast 
-# for a location and nearby trails with the distance to each trail.
-# current forecast for the start location
-# estimated travel time for each trail
