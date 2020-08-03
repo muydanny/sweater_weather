89604c2d29709344091e4a0ea571cc4dc7a13389
Danny Ramos <danramos21@gmail.com>
2020-08-03 11:35:00 -0600

    created new mapquest method

--- app/controllers/api/v1/trail_controller.rb
+++ app/controllers/api/v1/trail_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::TrailController < ApplicationController
     map_info = MapquestService.new.get_lat_long(params[:location])
     trail_info = HikingProjectService.new.get_trails(map_info)
     weather_info = OpenWeatherService.new.get_forecast(map_info)
-
-    render json: HikingProjectSerializer.new(Trail.new(trail_info, weather_info)).serialized_json
+    distance_info = MapquestService.new.get_distance(params[:location], trail_info)
+    render json: HikingProjectSerializer.new(Trail.new(trail_info, weather_info, distance_info)).serialized_json
   end
 end
