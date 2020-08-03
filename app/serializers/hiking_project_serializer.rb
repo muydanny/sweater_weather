dc85b3ea8c6ce5c275f181c7e06dd4c59fe480bc
Danny Ramos <danramos21@gmail.com>
2020-08-03 10:45:35 -0600

    added attributes to HP serializer

--- app/serializers/hiking_project_serializer.rb
+++ app/serializers/hiking_project_serializer.rb
@@ -1,5 +1,5 @@
 class HikingProjectSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
-  attributes :trail_name, :trail_summary, :trail_difficulty, :trail_location
+  attributes :trail_name, :trail_summary, :trail_difficulty, :trail_location, :current_location_weather_summary, :current_location_temp
 end
