7b9c4d051b32c9b75bb5c65df1dc6cdf0111ee4d
Danny Ramos <danramos21@gmail.com>
2020-08-03 10:36:46 -0600

    used open weather service for weather

--- app/controllers/api/v1/trail_controller.rb
+++ app/controllers/api/v1/trail_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::TrailController < ApplicationController
   def index
     map_info = MapquestService.new.get_lat_long(params[:location])
     trail_info = HikingProjectService.new.get_trails(map_info)
-    weather_info = 
+    weather_info = OpenWeatherService.new.get_forecast(map_info)
 
     render json: HikingProjectSerializer.new(Trail.new(trail_info, weather_info)).serialized_json
   end
