8819d04287f119e543886c64441f39944171ca6f
Danny Ramos <danramos21@gmail.com>
2020-08-03 10:12:53 -0600

    passing spec for trail info

--- spec/requests/api/v1/location_forecast_trails_spec.rb
+++ spec/requests/api/v1/location_forecast_trails_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
 describe "Forecast for nearby trails" do
-  it "can get a forecast for a nearby trail", :vcr do
+  it "can get a trails for a location", :vcr do
 
     get "/api/v1/trails?location=denver,co"
     expect(response).to be_successful  
+    body = JSON.parse(response.body)
     
+    expect(body["data"]["attributes"]["trail_name"]).to eq("Boulder Skyline Traverse")
+    expect(body["data"]["attributes"]["trail_summary"]).to eq("The classic long mountain route in Boulder.")
+    expect(body["data"]["attributes"]["trail_difficulty"]).to eq("black")
+    expect(body["data"]["attributes"]["trail_location"]).to eq("Superior, Colorado")
   end 
 end
 
+
+
 # You will build an endpoint that will retrieve the forecast 
 # for a location and nearby trails with the distance to each trail.
-
 # current forecast for the start location
-# name of the trail
-# summary of each trail
-# difficulty of each trail
-# location of each trail
 # estimated travel time for each trail
