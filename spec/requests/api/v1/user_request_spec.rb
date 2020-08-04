426c887601677338d022b788971bf28c1618522e
Danny Ramos <danramos21@gmail.com>
2020-08-03 22:39:01 -0600

    passing spec for new user

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -4,9 +4,16 @@ describe "Registration" do
   it "can register a new user" do
     user_params = { email: "whatever@example.com", password: "password", password_confirmation: "password"}
     post '/api/v1/users', params: {user: user_params}
-    user = User.last
+    
     expect(response).to be_successful  
-    expect(user.email).to eq(user_params[:email])
+    
+    json = JSON.parse(response.body)
+    user = User.last
+
+    expect(json["data"]["type"]).to eq("users")
+    expect(json["data"]["attributes"]["email"]).to eq(user.email)
+    expect(user.api_key).to be_a(String)
+    expect(response).to have_http_status(201)
   end 
 end
 
