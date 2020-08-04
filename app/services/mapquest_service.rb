79d445e97aa394e2ba179e68d4c1eba01edf580d
Danny Ramos <danramos21@gmail.com>
2020-08-04 16:13:17 -0600

    added road_trip method for mapquest service

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -8,6 +8,16 @@ class MapquestService
     json[:results][0][:locations][0][:latLng]
   end
 
+  def road_trip(origin, destination)
+    response = conn.get("/directions/v2/route") do |req|
+      req.params["key"] = ENV["MAP_KEY"]
+      req.params["from"] = origin
+      req.params["to"] = destination
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
+  end
+
   private
 
   def conn
