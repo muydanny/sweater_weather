55cff2f212722aed693c9afbbc3d071a8c069cf2
Danny Ramos <danramos21@gmail.com>
2020-08-04 16:44:06 -0600

    updated roadtrip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
+    require 'pry'; binding.pry
     user = User.where(params[:api_key]).first
-    road_trip_info = MapquestService.new.road_trip(params[:road_trip][:origin], params[:road_trip][:destination])
+    weather_info = OpenWeatherService.new.get_forecast(road_trip_params[:destination])
+    road_trip_info = MapquestService.new.road_trip(road_trip_params[:origin], road_trip_params[:destination])
+    destination = Destination.new(weather_info, road_trip_info)
+    
+    render json: RoadTripSerializer.new(Destination.new(weather_info, road_trip_info))
   end
 
   private
