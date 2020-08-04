3a5694a9b9ab467200683df9660f91aa03555e44
Danny Ramos <danramos21@gmail.com>
2020-08-04 09:47:51 -0600

    passing spec for user login

--- spec/requests/api/v1/user_login_request_spec.rb
+++ spec/requests/api/v1/user_login_request_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 describe "Login API" do
   it "can login a registered user" do
+    user = create(:user)
     user_params = { email: "whatever@example.com", password: "password"}
     post '/api/v1/sessions', params: {user: user_params}
     
     expect(response).to be_successful  
     
     json = JSON.parse(response.body)
-    user = User.last
-
+    
+    expect(json["data"]["type"]).to eq("users")
+    expect(json["data"]["attributes"]["email"]).to eq(user.email)
+    expect(user.api_key).to be_a(String)
+    expect(response).to have_http_status(201)
   end 
 end
 #   POST /api/v1/sessions
