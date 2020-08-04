069dc5611354d2cfab8a4cc7811d599c6e443dbb
Danny Ramos <danramos21@gmail.com>
2020-08-04 16:55:03 -0600

    refactored mapquest service

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -1,13 +1,13 @@
 class MapquestService
+  
   def get_lat_long(location)
     response = conn.get("/geocoding/v1/address") do |req|
-      req.params["key"] = ENV["MAP_KEY"]
       req.params["location"] = location
     end
     json = JSON.parse(response.body, symbolize_names: true)
     json[:results][0][:locations][0][:latLng]
   end
-
+  
   def road_trip(origin, destination)
     response = conn.get("/directions/v2/route") do |req|
       req.params["from"] = origin
@@ -16,11 +16,12 @@ class MapquestService
     json = JSON.parse(response.body, symbolize_names: true)
     require 'pry'; binding.pry
   end
-
+  
   private
+  BASE_URL = "http://www.mapquestapi.com".freeze
 
   def conn
-    Faraday.new("http://www.mapquestapi.com") do |req|
+    Faraday.new(BASE_URL) do |req|
       req.params["key"] = ENV["MAP_KEY"]
     end
   end
