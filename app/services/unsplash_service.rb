28d80406630975035181ea1600c201ee41e33e59
Danny Ramos <danramos21@gmail.com>
2020-08-04 16:55:45 -0600

    refactored unsplash service

--- app/services/unsplash_service.rb
+++ app/services/unsplash_service.rb
@@ -1,4 +1,5 @@
 class UnsplashService
+  
   def get_image(location)
     response = conn.get("/search/photos") do |req|
       req.params["query"] = location
@@ -6,11 +7,12 @@ class UnsplashService
     json = JSON.parse(response.body, symbolize_names: true)
     json[:results][0][:urls][:full]
   end
-
+  
   private
+  BASE_URL = "https://api.unsplash.com".freeze
 
   def conn
-    Faraday.new("https://api.unsplash.com") do |req|
+    Faraday.new(BASE_URL) do |req|
       req.params["client_id"] = ENV["UNSPLASH_KEY"]
       req.headers["Accept-Version"]= 'v1'
     end
