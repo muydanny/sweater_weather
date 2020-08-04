f145816d290fd3701093713d9e65b93d58b940e9
Danny Ramos <danramos21@gmail.com>
2020-08-04 16:49:42 -0600

    refactored forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,15 @@
-class Api::V1::ForecastController < ApplicationController
+  class Api::V1::ForecastController < ApplicationController
 
   def index
-    map_info = MapquestService.new.get_lat_long(params[:location])
-    open_weather_info = OpenWeatherService.new.get_forecast(map_info)
+    mapquest_response = MapquestService.new.get_lat_long(location_params[:location])
+    open_weather_response = OpenWeatherService.new.get_forecast(mapquest_response)
    
-    render json: OpenWeatherSerializer.new(Weather.new(open_weather_info)).serialized_json
+    render json: OpenWeatherSerializer.new(Weather.new(open_weather_response)).serialized_json
+  end
+
+  private
+  
+  def location_params
+    params.permit(:location)
   end
 end
