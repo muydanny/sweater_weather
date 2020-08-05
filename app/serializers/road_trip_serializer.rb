3cfe969427ac099694f6427c38de8198ad57f9a9
Danny Ramos <danramos21@gmail.com>
2020-08-04 22:48:02 -0600

    road trip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,4 +1,14 @@
 class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :origin, :destinaiton, :duration, :temp, :temp_description
+  set_id :id
+  attributes :road_trip_route,
+             :weather_info,
+             :road_trip_info,
+             :weather_info,
+             :road_trip_info,
+             :orign,
+             :destination,
+             :travel_time,
+             :arrival_forecast_temp,
+             :arrival_forecast_description
 end
